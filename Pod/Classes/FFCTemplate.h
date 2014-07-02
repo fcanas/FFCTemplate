f60a9381fa0ee2eeb5e03e8454add2eec6a4584a
Fabian Canas <fcanas@gmail.com>
2014-07-02 00:28:48 -0400

    Permitted parameters. [WIP]

--- Pod/Classes/FFCTemplate.h
+++ Pod/Classes/FFCTemplate.h
@@ -8,19 +8,36 @@
 
 #import <Foundation/Foundation.h>
 
+@protocol FFCTemplateSource <NSObject>
+@optional
+/**
+ An array of strings for keys, method names, and key paths that 
+ the template is permitted to ask of the source.
+ 
+ In the case that permittedKeys is nil, it is intended that all
+ keys are permitted. This behavior may change.
+ */
++ (NSArray *)permittedKeys;
+@end
+
 @interface FFCTemplate : NSObject
 
-@property (nonatomic, weak) id valueSource;
+/**
+ An object that has properties, methods, or key paths for the 
+ match strings in the template.
+ */
+@property (nonatomic, weak) id<FFCTemplateSource> valueSource;
+
 /**
  Returns a cached version of the rendered template if one exists.
  If no pre-rendered version exists, it will render the temlpate.
  */
--(NSString *)renderedTemplate;
+- (NSString *)renderedTemplate;
 
 - (instancetype)initWithTemplate:(NSString *)templateString;
 
-/* 
- Forces the FFCTemlate to render and returns the resulting string.
+/**
+ Forces the FFCTemplate to render and returns the resulting string.
  */
 - (NSString *)render;
 
--- Pod/Classes/FFCTemplate.m
+++ Pod/Classes/FFCTemplate.m
@@ -40,7 +40,14 @@
 - (NSString *)render
 {
     NSMutableString* destination = [[NSMutableString alloc] initWithCapacity:self.templateString.length * 4];
-    NSString* scannedString  = nil;
+    NSString *scannedString  = nil;
+    NSArray *permittedKeys = nil;
+    
+    if ([[self.valueSource class] respondsToSelector:@selector(permittedKeys)]) {
+        permittedKeys = [[self.valueSource class] permittedKeys];
+    } else {
+        // Consider warning the developer?
+    }
     
     NSScanner* scanner = [[NSScanner alloc] initWithString:self.templateString];
     scanner.charactersToBeSkipped = nil;
@@ -58,8 +65,15 @@
             
             [scanner scanUpToString:[self endDelimiter] intoString:&key];
             key = [key stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+            [scanner scanString:[self endDelimiter] intoString:nil];
             
-            id value = [self.valueSource valueForKeyPath:key];
+            // In the case that permittedKeys is nil, it is intended that all
+            // keys are permitted. This behavior may change.
+            if ([permittedKeys indexOfObject:key]==NSNotFound) {
+                continue;
+            }
+            
+            id value = [(NSObject *)self.valueSource valueForKeyPath:key];
             if ([value isKindOfClass:[NSString class]]) {
                 [destination appendString:value];
             } else if ([value respondsToSelector:@selector(stringValue)]) {
@@ -69,8 +83,6 @@
             } else {
                 
             }
-            
-            [scanner scanString:[self endDelimiter] intoString:nil];
         }
     }
     @catch (NSException *exception) {
